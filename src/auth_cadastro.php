7275d6e0175ce72fd29c547d83dae2b28bcf992d
João Guilherme <guilherme.lira019@gmail.com>
2025-09-05 20:13:36 -0300

    Atualização 05/09

---
+++ src/auth_cadastro.php
@@ -0,0 +1,41 @@
+<?php 
+// Inserindo cargo Master ou Porteiro
+require_once ('../config.php');
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $nome = filter_input(INPUT_POST, 'nome');
+    $cpf = filter_input(INPUT_POST, 'cpf');
+    $cargo = filter_input(INPUT_POST, 'cargo');
+    $senha = filter_input(INPUT_POST, 'senha');
+
+
+    $senha = password_hash($senha, PASSWORD_ARGON2ID);
+
+    $insertcargo = "INSERT INTO usuarios VALUES(null, :nome, :cpf, :senha, :cargo)";
+    $req = $dbh->prepare($insertcargo);
+    $req->bindValue(':nome', $nome);
+    $req->bindValue(':cpf', $cpf);
+    $req->bindValue(':senha', $senha);
+    $req->bindValue(':cargo', $cargo);
+    if($req->execute()){
+
+        header("location: ../login.php?sucesso=0");
+
+    }else{
+        header("Location: ..views_cadastro.php?sucesso=1");
+    }
+
+}else{
+    header("Location: ..views_cadastro.php?sucesso=1");
+    exit;
+}
+
+
+/* 
+1º Receber as informações do HTML via POST
+2º preparar a string de inserção 
+3º executar
+4º Exibir uma mensagem de cadastro realizador com sucesso.
+
+*/
+?>
--- src/auth_login.php
+++ src/auth_login.php
@@ -5,22 +5,25 @@ $cpf = $_POST['cpf'];
 $senha = $_POST['senha'];
 
 
-$verificaUsuario = "SELECT senha FROM usuarios WHERE cpf = :cpf";
+$verificaUsuario = "SELECT senha, cargo FROM usuarios WHERE cpf = :cpf";
 $req = $dbh->prepare($verificaUsuario);
 $req->bindValue(':cpf', $cpf);
 $req->execute();
 $dadosUsuario = $req->fetch(PDO::FETCH_ASSOC);
 
 if($dadosUsuario){
-    if(password_verify($senha, $dadosUsuario["senha"])){
-        header('Location: ../index_menu.php?sucesso=0');
-        exit;
-    }else{
-        header('Location: ../login.php?sucesso=1');
-        exit;
+    if (password_verify($senha, $dadosUsuario["senha"])) {
+        
+        if($dadosUsuario["cargo"] === "porteiro"){
+            header('Location: ../menu_porteiro.php?sucesso=0');
+        }
+        elseif($dadosUsuario["cargo"] === "master"){
+            header('Location: ../menu_master.php?sucesso=0');
+        }
+        else{
+            header('Location: ../login.php?sucesso=2');
+            exit();
+        }
     }
-    }else{
-    header('Location: ../login.php?sucesso=1');
-    exit;
 }
 ?>
